49b1e919ba3ea81f96d7c7312417a041719952ed
Prateek Patel <118043610+prateekP1906@users.noreply.github.com>
2026-04-19 09:30:14 +0530

    Fix formatting and remove unnecessary newline

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-
 import os, asyncio, humanize
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
@@ -204,4 +203,4 @@ async def delete_files(messages, client, k):
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
     await k.edit_text("Your Video / File Is Successfully Deleted ✅")
-        
+    
